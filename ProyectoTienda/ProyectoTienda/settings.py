d448f35d49196595864a7702e2da7734421f3707
Cesáreo Castillo <cesareocastillo@MacBook-Air-de-Cesareo.local>
2024-11-29 14:41:57 -0600

    files

--- ProyectoTienda/ProyectoTienda/settings.py
+++ ProyectoTienda/ProyectoTienda/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-=yhb(_@of&#b1fr_2t&!z&))oe4&(-i860e)&f27c*#o8z*+p('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 AUTH_USER_MODEL = "AppTienda.user"
